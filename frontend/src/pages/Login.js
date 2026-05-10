3951c8c677a32bda2a7f23faa80cb496c42a4849
unknown <e0323045@sriher.edu.in>
2026-05-10 12:54:21 +0530

    fixed deployed api urls

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   useEffect(() => {
     const checkBackend = async () => {
       try {
-        await fetch("http://localhost:8000/api/auth/login", {
+        await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email: "test", password: "test" }),
